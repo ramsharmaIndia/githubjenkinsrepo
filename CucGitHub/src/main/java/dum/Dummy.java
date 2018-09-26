5bf31cb75e72cc93ab8737ca64f941ec3dd90af9
Ram Sharma <ramsharma.india001@gmail.com>
2018-09-26 09:49:57 +0530

    some change

--- CucGitHub/src/main/java/dum/Dummy.java
+++ CucGitHub/src/main/java/dum/Dummy.java
@@ -3,6 +3,6 @@ package dum;
 public class Dummy {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("Go and win the world");
+		System.out.println("Go and win the world and come back");
 	}
 }
